f79fb20bd4ebce1eb12788e6657277b70c9b6288
Joris <jjahuijbregts@gmail.com>
2022-11-04 17:41:29 +0100

    Modified objdump file to extract size by namespace

--- src/bin/wasm-tools/objdump.rs
+++ src/bin/wasm-tools/objdump.rs
@@ -1,7 +1,9 @@
-use anyhow::Result;
-use std::io::Write;
+use anyhow::{Result, bail};
+use regex::Regex;
+use std::{io::Write, fmt};
 use std::ops::Range;
-use wasmparser::{Encoding, Parser, Payload::*, SectionReader};
+use std::collections::HashMap;
+use wasmparser::{Encoding, Parser, Payload::*, SectionReader, ValType, SectionWithLimitedItems};
 
 /// Dumps information about sections in a WebAssembly file.
 ///
@@ -11,6 +13,13 @@ use wasmparser::{Encoding, Parser, Payload::*, SectionReader};
 pub struct Opts {
     #[clap(flatten)]
     io: wasm_tools::InputOutput,
+}
+
+#[derive(Clone, Copy)]
+pub struct FunctionInfo {
+    id: u32,
+    callCount: u32,
+    byteSize: usize,
 }
 
 impl Opts {
@@ -23,26 +32,86 @@ impl Opts {
         };
         printer.indices.push(IndexSpace::default());
 
+        let mut functionInfo = Vec::new();
+        // let mut functionInfoMap = HashMap::new();
+        let mut functionNamesMap = HashMap::new();
+        let mut typesNameMap = HashMap::new();
+        let mut code_section_counter = 0;
+
         for payload in Parser::new(0).parse_all(&input) {
             match payload? {
                 Version { .. } => {}
 
-                TypeSection(s) => printer.section(s, "types")?,
+                TypeSection(s) => {
+
+                    
+                    printer.section(s, "types")?
+
+                },
                 ImportSection(s) => printer.section(s, "imports")?,
-                FunctionSection(s) => printer.section(s, "functions")?,
+                FunctionSection(mut s) => {
+
+                    let mut already_owned_count = 0;
+                    println!("Function section. Has: {} count", s.get_count());
+                    for _ in 0..s.get_count() {
+                        let id = s.read()?;
+                        functionInfo.push(FunctionInfo{ id: id, callCount: 0, byteSize: 0 });
+                    }
+                    if !s.eof() {
+                        bail!("too many bytes in section");
+                    }
+
+                    println!("Unique func signatures: {} - Duplicate function signatures {}", functionInfo.len(), already_owned_count);
+
+                    printer.section(s, "functions")?
+                },
                 TableSection(s) => printer.section(s, "tables")?,
                 MemorySection(s) => printer.section(s, "memories")?,
                 TagSection(s) => printer.section(s, "tags")?,
-                GlobalSection(s) => printer.section(s, "globals")?,
+                GlobalSection(mut s) => {
+                    
+                    for _ in 0..s.get_count() {
+                        let result = s.read()?;
+                        println!("Global type: {:?} (mutable: {})", result.ty.content_type, result.ty.mutable);
+                    }
+
+                    printer.section(s, "globals")?;
+                },
                 ExportSection(s) => printer.section(s, "exports")?,
                 StartSection { range, .. } => printer.section_raw(range, 1, "start")?,
-                ElementSection(s) => printer.section(s, "elements")?,
+                ElementSection(mut s) => {
+                    
+                    for _ in 0..s.get_count() {
+                        let res = s.read()?;
+                        match res.kind {
+                            wasmparser::ElementKind::Passive => {
+                                // Ignored
+                            },
+                            wasmparser::ElementKind::Active { table_index, offset_expr } => {
+                                // let reader = offset_expr.get_binary_reader();
+                                // reader.
+                                println!("Active type. Table index {}", table_index);
+                            },
+                            wasmparser::ElementKind::Declared => {
+                                if res.ty == ValType::FuncRef {
+                                    // println!("Func declared. Items count {}");
+                                }
+                            },
+                        }
+                    }
+
+
+                    printer.section(s, "elements")?
+                },
                 DataCountSection { range, .. } => printer.section_raw(range, 1, "data count")?,
                 DataSection(s) => printer.section(s, "data")?,
                 CodeSectionStart { range, count, .. } => {
                     printer.section_raw(range, count, "code")?
                 }
-                CodeSectionEntry(_) => {}
+                CodeSectionEntry(mut s) => {
+                    functionInfo[code_section_counter].byteSize = s.range().end - s.range().start;
+                    code_section_counter += 1;
+                }
 
                 ModuleSection { range, .. } => {
                     printer.section_raw(range, 1, "module")?;
@@ -63,16 +132,165 @@ impl Opts {
                 ComponentImportSection(s) => printer.section(s, "component imports")?,
                 ComponentExportSection(s) => printer.section(s, "component exports")?,
                 
-                CustomSection(c) => printer.section_raw(
-                    c.data_offset()..c.data_offset() + c.data().len(),
-                    1,
-                    &format!("custom {:?}", c.name()),
-                )?,
+                CustomSection(c) => {
+                    if c.name() == "name" {
+                        println!("custom section name: {}", c.name());
+                        
+                        // let mut functionNameCount = 0;
+                        // let mut localNameCount = 0;
+                        // let mut labelNameCount = 0;
+                        // let mut typeNameCount = 0;
+                        // let mut tableNameCount = 0;
+                        // let mut memoryNameCount = 0;
+                        // let mut globalNameCount = 0;
+                        // let mut elementNameCount = 0;
+                        // let mut dataNameCount = 0;
+
+                        let mut nameiter = wasmparser::NameSectionReader::new(c.data(), c.data_offset())?;
+                        while !nameiter.eof() {
+                            let name = nameiter.read()?;
+                            let range = nameiter.original_position();
+
+                            match name {
+                                wasmparser::Name::Module { name, name_range } => {
+                                    println!("module name");
+                                    println!("{}", name_range.start);
+                                    println!("{:?}", name);
+                                    println!("{}", name_range.end);
+                                }
+                                wasmparser::Name::Function(mut n) => {
+                                    for _ in 0..n.get_count() {
+                                        let item = n.read()?;
+                                        if functionNamesMap.contains_key(&item.index) {
+                                            bail!("function name already in lookup");
+                                        }
+
+                                        functionNamesMap.insert(item.index, item.name);
+                                    }
+                                    if !n.eof() {
+                                        bail!("too many bytes in section");
+                                    }
+                                },
+                                wasmparser::Name::Local(n) => {
+                                    // self.print_indirect_name_map("function", "local", n)?
+                                },
+                                wasmparser::Name::Label(n) => {
+                                    // self.print_indirect_name_map("function", "label", n)?
+                                },
+                                wasmparser::Name::Type(mut n) => {
+                                    for _ in 0..n.get_count() {
+                                        let item = n.read()?;
+                                        if typesNameMap.contains_key(&item.index) {
+                                            bail!("type name already in lookup");
+                                        }
+
+                                        typesNameMap.insert(item.index, item.name);
+                                    }
+                                    if !n.eof() {
+                                        bail!("too many bytes in section");
+                                    }
+
+                                    // self.print_name_map("type", n)?
+                                },
+                                wasmparser::Name::Table(n) => {
+                                    // self.print_name_map("table", n)?
+                                },
+                                wasmparser::Name::Memory(n) => {
+                                    // self.print_name_map("memory", n)?
+                                },
+                                wasmparser::Name::Global(n) => {
+                                    // self.print_name_map("global", n)?
+                                },
+                                wasmparser::Name::Element(n) => {
+                                    // self.print_name_map("element", n)?
+                                },
+                                wasmparser::Name::Data(n) => {
+                                    // self.print_name_map("data", n)?
+                                },
+                                wasmparser::Name::Unknown { ty, range, .. } => {
+                                    // write!(self.state, "unknown names: {}", ty)?;
+                                    // self.print(range.start)?;
+                                    // self.print(end)?;
+                                }
+                            }
+                        }
+                    }
+
+                    printer.section_raw(
+                        c.data_offset()..c.data_offset() + c.data().len(),
+                        1,
+                        &format!("custom {:?}", c.name()),
+                    )?
+                },
 
                 UnknownSection { .. } => {}
 
                 End(_) => printer.end()?,
             }
+        }
+
+
+        let mut funcs_by_namespace = HashMap::new();
+        funcs_by_namespace.insert("non_namespaced", Vec::new());
+
+        let namespace_reg = Regex::new(r"(?:\s?)([a-zA-Z0-9-_]+)(?:::)+?").expect("regex failed to compile");
+        for index in 0..functionInfo.len() {
+            let index_u32 = index as u32;
+            let info = functionInfo[index];
+            let name = functionNamesMap.get(&index_u32).expect("Couldn't find key in name");
+            
+            let captures_a = namespace_reg.captures(name);
+            match captures_a {
+                Some(captures) => {
+                    let namespace = captures.get(1).unwrap().as_str();
+    
+                    if funcs_by_namespace.contains_key(namespace) {
+                        let v : &mut Vec<FunctionInfo> = funcs_by_namespace.get_mut(namespace).unwrap();
+                        v.push(info);
+                    } else {
+                        let mut v = Vec::new();
+                        v.push(info);
+                        funcs_by_namespace.insert(namespace, v);
+                    }
+                }
+                None => {
+                    let v : &mut Vec<FunctionInfo> = funcs_by_namespace.get_mut("non_namespaced").unwrap();
+                    v.push(info);
+                }
+            }
+        }
+
+        let mut namespaces_ordered : Vec<(&&str, &Vec<FunctionInfo>)> = funcs_by_namespace.iter().collect();
+        namespaces_ordered.sort_by(|a, b| {
+            let mut byteSizeA = 0usize;
+            for k in a.1 {
+                byteSizeA += k.byteSize;
+            }
+
+            let mut byteSizeB = 0usize;
+            for k in b.1 {
+                byteSizeB += k.byteSize;
+            }
+            
+            if byteSizeA == byteSizeB {
+                return std::cmp::Ordering::Equal;
+            }
+
+            if byteSizeA > byteSizeB {
+                return std::cmp::Ordering::Greater;
+            }
+
+            return std::cmp::Ordering::Less;
+        });
+
+        println!("{} namespaces found", namespaces_ordered.len());
+        for tuple in namespaces_ordered {
+            let mut byteSize = 0usize;
+            for k in tuple.1 {
+                byteSize += k.byteSize;
+            }
+
+            println!("'{}' functions: {} ({} bytes)", tuple.0, tuple.1.len(), byteSize);
         }
 
         Ok(())
